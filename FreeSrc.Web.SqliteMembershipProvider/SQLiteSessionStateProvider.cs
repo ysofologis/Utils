04127535d2b7d3265396cb391f5f679d98e11132
Yiannis <yiannis_sofologis@hotmail.com>
2014-02-14 12:23:07 +0200

    sqlite membership aspx

---
+++ FreeSrc.Web.SqliteMembershipProvider/SQLiteSessionStateProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeSrc.Web.Providers
+{
+    public class SQLiteSessionStateProvider
+    {
+    }
+}
